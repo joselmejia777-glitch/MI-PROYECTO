074398e56b7d2c730ca64225b05a99fb700068e4
joselmejia777-glitch <joselmejia777@gmail.com>
2026-09-15 17:04:29 -0500

    Update conexion.js
    Limitar conexiones de la base de datos

--- api-rest/conexion.js
+++ api-rest/conexion.js
@@ -5,7 +5,8 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'consultorio_saludpro',
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+    connectionLimit: 2
 });
 
 module.exports = pool.promise();
